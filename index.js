f291326a366943eaa4735adaf66d5da2850a0be4
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-10 22:37:32 +0000

    lightcoin crypto OOP assignment complete

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    if (this.isAllowed()) {
+      this.account.addTransaction(this.value);
+    } else {
+      console.log("Insufficient funds");
+    }
+  }
+
+  isAllowed() {
+    if (this.account.balance + this.value < 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // this.balance = 0;
+    //this.transactions is an array of Transaction objects.
+    //So we need to access the object's amount in the get balance method
+    this.transactions = [0];
+  }
+  get balance() {
+    const balance = this.transactions.reduce((a, t) => {
+      return a + t;
+    })
+    return balance;
+
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+const myAccount = new Account('grep');
 
+const t0 = new Deposit(120, myAccount);
+t0.commit();
 
-
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log("Balance:", myAccount.balance);
